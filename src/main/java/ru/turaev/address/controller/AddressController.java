e49af91c73119948d41d40980e8d7bea710dcd39
Turaev <denis.turaev@bk.ru>
2022-08-07 01:34:10 +0300

    Added the ability to log using Log4j2. Logs are sent to the console and ELK.

--- src/main/java/ru/turaev/address/controller/AddressController.java
+++ src/main/java/ru/turaev/address/controller/AddressController.java
@@ -1,5 +1,6 @@
 package ru.turaev.address.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import ru.turaev.address.model.Address;
 import ru.turaev.address.service.AddressService;
 
+@Slf4j
 @RestController
 @RequestMapping("api/v1/addresses")
 public class AddressController {
@@ -18,6 +20,9 @@ public class AddressController {
 
     @PostMapping
     public Address addAddress(@RequestBody Address address) {
-        return addressService.addAddress(address);
+        log.info("Attempt to save a new address in the database");
+        Address address1 = addressService.addAddress(address);
+        log.info("Address with id {} was created", address1.getId());
+        return address1;
     }
 }
