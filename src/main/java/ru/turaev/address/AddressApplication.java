b6f530cebe9a4a47e7348cf3b91f39324e440b1f
Turaev <denis.turaev@bk.ru>
2022-08-09 19:59:28 +0300

    Added Spring Boot Actuator and Spring Cloud Config

--- src/main/java/ru/turaev/address/AddressApplication.java
+++ src/main/java/ru/turaev/address/AddressApplication.java
@@ -2,7 +2,9 @@ package ru.turaev.address;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
+@RefreshScope
 @SpringBootApplication
 public class AddressApplication {
     public static void main(String[] args) {
