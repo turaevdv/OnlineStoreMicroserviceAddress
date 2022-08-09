12322c0e01597e21063180e3d9d85646a9bca119
Turaev <denis.turaev@bk.ru>
2022-08-09 23:11:39 +0300

    The service has become a Eureka client

--- src/main/java/ru/turaev/address/AddressApplication.java
+++ src/main/java/ru/turaev/address/AddressApplication.java
@@ -3,8 +3,10 @@ package ru.turaev.address;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @RefreshScope
+@EnableEurekaClient
 @SpringBootApplication
 public class AddressApplication {
     public static void main(String[] args) {
